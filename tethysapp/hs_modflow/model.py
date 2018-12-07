d7918a5b3559c76e8a2beb9b0712b67e00444dcf
krewgar <coreykrewson@gmail.com>
2018-12-07 15:29:11 -0700

    created all tables that could be used and added foreign keys to models table. All text files are stored in appropriate tables with data as a string

--- tethysapp/hs_modflow/model.py
+++ tethysapp/hs_modflow/model.py
@@ -18,11 +18,544 @@ class Model(Base):
     __tablename__ = 'models'
 
     # Columns
-    resourceid = Column(String, primary_key=True)
+    id = Column(Integer, primary_key=True)
+    resourceid = Column(String)
     displayname = Column(String)
     modeltype = Column(String)
     modelfiles = Column(String)
+    zoneid = Column(Integer, ForeignKey("zone.id"), nullable=True)
+    multid = Column(Integer, ForeignKey("mult.id"), nullable=True)
+    pvalid = Column(Integer, ForeignKey("pval.id"), nullable=True)
+    bas6id = Column(Integer, ForeignKey("bas6.id"), nullable=True)
+    disid = Column(Integer, ForeignKey("dis.id"), nullable=True)
+    disuid = Column(Integer, ForeignKey("disu.id"), nullable=True)
+    bcf6id = Column(Integer, ForeignKey("bcf6.id"), nullable=True)
+    lpfid = Column(Integer, ForeignKey("lpf.id"), nullable=True)
+    hfb6id = Column(Integer, ForeignKey("hfb6.id"), nullable=True)
+    chdid = Column(Integer, ForeignKey("chd.id"), nullable=True)
+    fhbid = Column(Integer, ForeignKey("fhb.id"), nullable=True)
+    welid = Column(Integer, ForeignKey("wel.id"), nullable=True)
+    mnw1id = Column(Integer, ForeignKey("mnw1.id"), nullable=True)
+    mnw2id = Column(Integer, ForeignKey("mnw2.id"), nullable=True)
+    mnwiid = Column(Integer, ForeignKey("mnwi.id"), nullable=True)
+    drnid = Column(Integer, ForeignKey("drn.id"), nullable=True)
+    rchid = Column(Integer, ForeignKey("rch.id"), nullable=True)
+    evtid = Column(Integer, ForeignKey("evt.id"), nullable=True)
+    ghbid = Column(Integer, ForeignKey("ghb.id"), nullable=True)
+    gmgid = Column(Integer, ForeignKey("gmg.id"), nullable=True)
+    lmt6id = Column(Integer, ForeignKey("lmt6.id"), nullable=True)
+    lmt7id = Column(Integer, ForeignKey("lmt7.id"), nullable=True)
+    rivid = Column(Integer, ForeignKey("riv.id"), nullable=True)
+    strid = Column(Integer, ForeignKey("str.id"), nullable=True)
+    swi2id = Column(Integer, ForeignKey("swi2.id"), nullable=True)
+    pcgid = Column(Integer, ForeignKey("pcg.id"), nullable=True)
+    pcgnid = Column(Integer, ForeignKey("pcgn.id"), nullable=True)
+    nwtid = Column(Integer, ForeignKey("nwt.id"), nullable=True)
+    pksid = Column(Integer, ForeignKey("pks.id"), nullable=True)
+    smsid = Column(Integer, ForeignKey("sms.id"), nullable=True)
+    sfrid = Column(Integer, ForeignKey("sfr.id"), nullable=True)
+    lakid = Column(Integer, ForeignKey("lak.id"), nullable=True)
+    gageid = Column(Integer, ForeignKey("gage.id"), nullable=True)
+    sipid = Column(Integer, ForeignKey("sip.id"), nullable=True)
+    sorid = Column(Integer, ForeignKey("sor.id"), nullable=True)
+    de4id = Column(Integer, ForeignKey("de4.id"), nullable=True)
+    ocid = Column(Integer, ForeignKey("oc.id"), nullable=True)
+    uzfid = Column(Integer, ForeignKey("uzf.id"), nullable=True)
+    upwid = Column(Integer, ForeignKey("upw.id"), nullable=True)
+    subid = Column(Integer, ForeignKey("sub.id"), nullable=True)
+    swtid = Column(Integer, ForeignKey("swt.id"), nullable=True)
+    hydid = Column(Integer, ForeignKey("hyd.id"), nullable=True)
+    hobid = Column(Integer, ForeignKey("hob.id"), nullable=True)
+    vdfid = Column(Integer, ForeignKey("vdf.id"), nullable=True)
+    vscid = Column(Integer, ForeignKey("vsc.id"), nullable=True)
 
+class zone(Base):
+    """
+    SQLAlchemy Model DB Model
+    """
+    __tablename__ = 'zone'
+
+    # Columns
+    id = Column(Integer, primary_key=True)
+    data = Column(String)
+
+class mult(Base):
+    """
+    SQLAlchemy Model DB Model
+    """
+    __tablename__ = 'mult'
+
+    # Columns
+    id = Column(Integer, primary_key=True)
+    data = Column(String)
+
+class pval(Base):
+    """
+    SQLAlchemy Model DB Model
+    """
+    __tablename__ = 'pval'
+
+    # Columns
+    id = Column(Integer, primary_key=True)
+    data = Column(String)
+
+
+class bas6(Base):
+    """
+    SQLAlchemy Model DB Model
+    """
+    __tablename__ = 'bas6'
+
+    # Columns
+    id = Column(Integer, primary_key=True)
+    data = Column(String)
+
+
+class dis(Base):
+    """
+    SQLAlchemy Model DB Model
+    """
+    __tablename__ = 'dis'
+
+    # Columns
+    id = Column(Integer, primary_key=True)
+    data = Column(String)
+
+
+class disu(Base):
+    """
+    SQLAlchemy Model DB Model
+    """
+    __tablename__ = 'disu'
+
+    # Columns
+    id = Column(Integer, primary_key=True)
+    data = Column(String)
+
+
+class bcf6(Base):
+    """
+    SQLAlchemy Model DB Model
+    """
+    __tablename__ = 'bcf6'
+
+    # Columns
+    id = Column(Integer, primary_key=True)
+    data = Column(String)
+
+
+class lpf(Base):
+    """
+    SQLAlchemy Model DB Model
+    """
+    __tablename__ = 'lpf'
+
+    # Columns
+    id = Column(Integer, primary_key=True)
+    data = Column(String)
+
+
+class hfb6(Base):
+    """
+    SQLAlchemy Model DB Model
+    """
+    __tablename__ = 'hfb6'
+
+    # Columns
+    id = Column(Integer, primary_key=True)
+    data = Column(String)
+
+
+class chd(Base):
+    """
+    SQLAlchemy Model DB Model
+    """
+    __tablename__ = 'chd'
+
+    # Columns
+    id = Column(Integer, primary_key=True)
+    data = Column(String)
+
+
+class fhb(Base):
+    """
+    SQLAlchemy Model DB Model
+    """
+    __tablename__ = 'fhb'
+
+    # Columns
+    id = Column(Integer, primary_key=True)
+    data = Column(String)
+
+
+class wel(Base):
+    """
+    SQLAlchemy Model DB Model
+    """
+    __tablename__ = 'wel'
+
+    # Columns
+    id = Column(Integer, primary_key=True)
+    data = Column(String)
+
+
+class mnw1(Base):
+    """
+    SQLAlchemy Model DB Model
+    """
+    __tablename__ = 'mnw1'
+
+    # Columns
+    id = Column(Integer, primary_key=True)
+    data = Column(String)
+
+
+class mnw2(Base):
+    """
+    SQLAlchemy Model DB Model
+    """
+    __tablename__ = 'mnw2'
+
+    # Columns
+    id = Column(Integer, primary_key=True)
+    data = Column(String)
+
+
+class mnwi(Base):
+    """
+    SQLAlchemy Model DB Model
+    """
+    __tablename__ = 'mnwi'
+
+    # Columns
+    id = Column(Integer, primary_key=True)
+    data = Column(String)
+
+
+class drn(Base):
+    """
+    SQLAlchemy Model DB Model
+    """
+    __tablename__ = 'drn'
+
+    # Columns
+    id = Column(Integer, primary_key=True)
+    data = Column(String)
+
+
+class rch(Base):
+    """
+    SQLAlchemy Model DB Model
+    """
+    __tablename__ = 'rch'
+
+    # Columns
+    id = Column(Integer, primary_key=True)
+    data = Column(String)
+
+
+class evt(Base):
+    """
+    SQLAlchemy Model DB Model
+    """
+    __tablename__ = 'evt'
+
+    # Columns
+    id = Column(Integer, primary_key=True)
+    data = Column(String)
+
+
+class ghb(Base):
+    """
+    SQLAlchemy Model DB Model
+    """
+    __tablename__ = 'ghb'
+
+    # Columns
+    id = Column(Integer, primary_key=True)
+    data = Column(String)
+
+
+class gmg(Base):
+    """
+    SQLAlchemy Model DB Model
+    """
+    __tablename__ = 'gmg'
+
+    # Columns
+    id = Column(Integer, primary_key=True)
+    data = Column(String)
+
+
+class lmt6(Base):
+    """
+    SQLAlchemy Model DB Model
+    """
+    __tablename__ = 'lmt6'
+
+    # Columns
+    id = Column(Integer, primary_key=True)
+    data = Column(String)
+
+
+class lmt7(Base):
+    """
+    SQLAlchemy Model DB Model
+    """
+    __tablename__ = 'lmt7'
+
+    # Columns
+    id = Column(Integer, primary_key=True)
+    data = Column(String)
+
+
+class riv(Base):
+    """
+    SQLAlchemy Model DB Model
+    """
+    __tablename__ = 'riv'
+
+    # Columns
+    id = Column(Integer, primary_key=True)
+    data = Column(String)
+
+
+class str(Base):
+    """
+    SQLAlchemy Model DB Model
+    """
+    __tablename__ = 'str'
+
+    # Columns
+    id = Column(Integer, primary_key=True)
+    data = Column(String)
+
+
+class swi2(Base):
+    """
+    SQLAlchemy Model DB Model
+    """
+    __tablename__ = 'swi2'
+
+    # Columns
+    id = Column(Integer, primary_key=True)
+    data = Column(String)
+
+
+class pcg(Base):
+    """
+    SQLAlchemy Model DB Model
+    """
+    __tablename__ = 'pcg'
+
+    # Columns
+    id = Column(Integer, primary_key=True)
+    data = Column(String)
+
+
+class pcgn(Base):
+    """
+    SQLAlchemy Model DB Model
+    """
+    __tablename__ = 'pcgn'
+
+    # Columns
+    id = Column(Integer, primary_key=True)
+    data = Column(String)
+
+class nwt(Base):
+    """
+    SQLAlchemy Model DB Model
+    """
+    __tablename__ = 'nwt'
+
+    # Columns
+    id = Column(Integer, primary_key=True)
+    data = Column(String)
+
+class pks(Base):
+    """
+    SQLAlchemy Model DB Model
+    """
+    __tablename__ = 'pks'
+
+    # Columns
+    id = Column(Integer, primary_key=True)
+    data = Column(String)
+
+
+class sms(Base):
+    """
+    SQLAlchemy Model DB Model
+    """
+    __tablename__ = 'sms'
+
+    # Columns
+    id = Column(Integer, primary_key=True)
+    data = Column(String)
+
+
+class sfr(Base):
+    """
+    SQLAlchemy Model DB Model
+    """
+    __tablename__ = 'sfr'
+
+    # Columns
+    id = Column(Integer, primary_key=True)
+    data = Column(String)
+
+
+class lak(Base):
+    """
+    SQLAlchemy Model DB Model
+    """
+    __tablename__ = 'lak'
+
+    # Columns
+    id = Column(Integer, primary_key=True)
+    data = Column(String)
+
+
+class gage(Base):
+    """
+    SQLAlchemy Model DB Model
+    """
+    __tablename__ = 'gage'
+
+    # Columns
+    id = Column(Integer, primary_key=True)
+    data = Column(String)
+
+
+class sip(Base):
+    """
+    SQLAlchemy Model DB Model
+    """
+    __tablename__ = 'sip'
+
+    # Columns
+    id = Column(Integer, primary_key=True)
+    data = Column(String)
+
+
+class sor(Base):
+    """
+    SQLAlchemy Model DB Model
+    """
+    __tablename__ = 'sor'
+
+    # Columns
+    id = Column(Integer, primary_key=True)
+    data = Column(String)
+
+
+class de4(Base):
+    """
+    SQLAlchemy Model DB Model
+    """
+    __tablename__ = 'de4'
+
+    # Columns
+    id = Column(Integer, primary_key=True)
+    data = Column(String)
+
+
+class oc(Base):
+    """
+    SQLAlchemy Model DB Model
+    """
+    __tablename__ = 'oc'
+
+    # Columns
+    id = Column(Integer, primary_key=True)
+    data = Column(String)
+
+
+class uzf(Base):
+    """
+    SQLAlchemy Model DB Model
+    """
+    __tablename__ = 'uzf'
+
+    # Columns
+    id = Column(Integer, primary_key=True)
+    data = Column(String)
+
+
+class upw(Base):
+    """
+    SQLAlchemy Model DB Model
+    """
+    __tablename__ = 'upw'
+
+    # Columns
+    id = Column(Integer, primary_key=True)
+    data = Column(String)
+
+
+class sub(Base):
+    """
+    SQLAlchemy Model DB Model
+    """
+    __tablename__ = 'sub'
+
+    # Columns
+    id = Column(Integer, primary_key=True)
+    data = Column(String)
+
+
+class swt(Base):
+    """
+    SQLAlchemy Model DB Model
+    """
+    __tablename__ = 'swt'
+
+    # Columns
+    id = Column(Integer, primary_key=True)
+    data = Column(String)
+
+
+class hyd(Base):
+    """
+    SQLAlchemy Model DB Model
+    """
+    __tablename__ = 'hyd'
+
+    # Columns
+    id = Column(Integer, primary_key=True)
+    data = Column(String)
+
+
+class hob(Base):
+    """
+    SQLAlchemy Model DB Model
+    """
+    __tablename__ = 'hob'
+
+    # Columns
+    id = Column(Integer, primary_key=True)
+    data = Column(String)
+
+class vdf(Base):
+    """
+    SQLAlchemy Model DB Model
+    """
+    __tablename__ = 'vdf'
+
+    # Columns
+    id = Column(Integer, primary_key=True)
+    data = Column(String)
+
+class vsc(Base):
+    """
+    SQLAlchemy Model DB Model
+    """
+    __tablename__ = 'vsc'
+
+    # Columns
+    id = Column(Integer, primary_key=True)
+    data = Column(String)
 
 def init_primary_db(engine, first_time):
     """
@@ -77,28 +610,9 @@ def save_hs_to_favorites(resourceid, displayname, modeltype):
     for resource in resourcelist:
         url = resource['url'].split("/")
         fname = url[-1]
-        ext = url[-1].split(".")[1]
         hs.getResourceFile('21c38e32c8f34de1a3073e738e7726bc', fname, destination=app_dir)
         filelist.append(fname)
-        with open(
-                '/Users/student/tethysdev/tethysapp-hs_modflow/tethysapp/hs_modflow/workspaces/app_workspace/etsdrt.dis',
-                'r'
-        ) as myfile:
-            data = myfile.read()
-            json.dumps(data)
 
-        table = Table(ext, meta,
-                          Column('resourceid', String, primary_key=True),
-                          Column('data', String)
-                          )
-        table.create(engine, checkfirst=True)
-
-        ins = table.insert().values(
-            resourceid=resourceid,
-            data=data)
-        conn.execute(ins)
-
-    conn.close()
     json.dumps(filelist)
 
     fav = Model(
@@ -111,6 +625,31 @@ def save_hs_to_favorites(resourceid, displayname, modeltype):
     # Add the model to the session, commit, and close
     session.add(fav)
 
+    model = session.query(Model).filter(Model.resourceid==resourceid).first()
+    mainid = model.id
+
+    for fi in filelist:
+        ext = fi.split(".")[1]
+        setattr(model, ext + 'id', mainid)
+        with open(
+                '/Users/student/tethysdev/tethysapp-hs_modflow/tethysapp/hs_modflow/workspaces/app_workspace/' + fi,
+                'r'
+        ) as myfile:
+            data = myfile.read()
+            json.dumps(data)
+
+        table = Table(ext, meta,
+                          Column('id', String, primary_key=True),
+                          Column('data', String)
+                          )
+        table.create(engine, checkfirst=True)
+
+        ins = table.insert().values(
+            id=mainid,
+            data=data)
+        conn.execute(ins)
+
+    conn.close()
     session.commit()
     session.close()
 
